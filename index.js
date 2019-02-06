0f740eefde09bee33960e18226f953d7b324e954
Reza Ghasemzadeh <rezaghasemzadeh1@gmail.com>
2019-02-06 14:29:38 +0330

    bux fixed cookie delete

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ let Cookie = (function () {
         },
         delete : (url,name)=>{
             let currentWindow = remote.getCurrentWindow();
-            currentWindow.webContents.session.cookies.remove(url,name,()=>{
-
+            currentWindow.webContents.session.cookies.remove(String(url),name,(error)=>{
+                console.error(error);
             });
         }
     }
